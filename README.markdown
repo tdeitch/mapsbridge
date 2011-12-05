2ff6ab4c0de7b767d2f21d09274347e41fa135f0
Trey Deitch <me@tdeitch.com>
2011-12-04 19:11:53 -0800

    Rewrote mapsbridge in ruby

--- README.markdown
+++ README.markdown
@@ -1,11 +1,11 @@
-Google My Maps for iPad
-=======================
+Mapsbridge
+==========
 
-Google Maps includes a feature called My Maps, which allows users to save maps with multiple locations. They are useful for planning trips, noting good restaurants, etc. These maps can be viewed on the iPad's Google Maps app, but only if the URL is in a special format. Furthermore, there's no easy way to get a list of a user's My Maps on the iPad. This web application fixes all that. When a user logs in, they are presented with a list of their maps. Clicking the name of a map opens it in the iPad's Maps app. It's that easy.
+Google Maps includes a feature called My Places, which allows users to save maps with multiple locations. They are useful for planning trips, noting good restaurants, etc. These maps can be viewed on the iPad's Google Maps app, but only if the URL is in a special format. Furthermore, there's no easy way to get a list of a user's My Places on the iPad. This web application fixes all that. When a user logs in, they are presented with a list of their maps. Clicking the name of a map opens it in the iPad's Maps app. It's that easy.
 
 Installation
 ------------
-The standalone (non-web) version of the software requires python with mechanize to run. The web version of the software also requires web.py. Beyond those prerequisites, there is no formal installation procedure. This page (http://webpy.org/install#dev) explains how to integrate web.py with a webserver.
+The software requires ruby with mechanize and sinatra.
 
 License
 -------
---
+++ maps.rb
@@ -0,0 +1,48 @@
+require 'rubygems'
+require 'mechanize'
+require 'sinatra'
+
+def get_links(username, password)
+  # Create a browser session using mechanize called agent
+  agent = Mechanize.new
+
+  # Open the login page, get the user's credentials, and sign-in to their Google account
+  page = agent.get("https://www.google.com/accounts/ServiceLogin?service=mobile&passive=true&cd=US&hl=en&continue=http://www.google.com/m&followup=http://www.google.com/m&btmpl=mobile_tier2")
+  login_form = page.forms.first
+  login_form.Email = username
+  login_form.Passwd = password
+  page = agent.submit(login_form)
+
+  if not page.search('div[@class="errormsg"]').empty?
+    return "</ul>The username or password you entered is incorrect.<ul>"
+  end
+
+  # Save each map and its URL
+  titles = []
+  links = []
+
+  # Get the URL of the users My Profile page, which contains a list of the users maps, and put it in maps_url
+  page = agent.get("http://maps.google.com/maps/user")
+  msids = ''
+  page = agent.page.link_with(:text => 'Maps Profile').click
+  page = agent.get(agent.page.uri.to_s + '&ptab=2')
+  page.links_with(:text => %r{» }, :href => %r{msid}).each do |title|
+    titles << title.text
+    links << title.uri.to_s
+  end
+  for i in (0..titles.length-1)
+    msids << '<li><a href="maps:q=http%3A//maps.google.com/maps/ms%3Fhl%3Den%26ie%3DUTF8%26oe%3DUTF8%26msa%3D0%26output%3Dkml%26msid%3D' + %r{msid=(.*)$}.match(links[i])[1] + '">'+titles[i]+'</a></li>'
+  end
+  return msids
+end
+
+get '/' do
+  erb :index
+end
+
+post '/' do
+  username = params[:username]
+  password = params[:password]
+  erb :maps,
+  :locals => {:links => get_links(username, password)}
+end
